8372b2168086ab88e39f0c2eca0e173e18433efc
Maciek Kwiatkowski <kwiatkowskimack@gmail.com>
2019-06-26 08:11:33 +1000

    added ability to search logs

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -6,7 +6,8 @@ import {
   DELETE_LOG,
   SET_CURRENT,
   CLEAR_CURRENT,
-  UPDATE_LOG
+  UPDATE_LOG,
+  SEARCH_LOGS
 } from './types';
 
 // get logs
@@ -103,6 +104,25 @@ export const updateLog = log => async dispatch => {
   }
 };
 
+// search logs
+export const searchLogs = text => async dispatch => {
+  try {
+    setLoading();
+
+    const res = await fetch(`/logs?q=${text}`);
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    });
+  }
+};
 // set current log
 export const setCurrent = log => {
   return {
