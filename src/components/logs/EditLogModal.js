1996bef138e7b587703746ea1920c2e2a3b07e83
Maciek Kwiatkowski <kwiatkowskimack@gmail.com>
2019-06-26 10:06:38 +1000

    added option to add and delete technicians

--- src/components/logs/EditLogModal.js
+++ src/components/logs/EditLogModal.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import M from 'materialize-css/dist/js/materialize.min.js';
+import TechSelectOptions from '../techs/TechSelectOptions';
 import { updateLog } from '../../actions/logActions';
 
 const EditLogModal = ({ current, updateLog }) => {
@@ -65,9 +66,7 @@ const EditLogModal = ({ current, updateLog }) => {
               <option value='' disabled>
                 Select technician
               </option>
-              <option value='John Doe'>John Doe</option>
-              <option value='Sam Smith'>Sam Smith</option>
-              <option value='Some Person'>Some Person</option>
+              <TechSelectOptions />
             </select>
           </div>
         </div>
